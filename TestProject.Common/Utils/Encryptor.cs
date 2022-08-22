58d3784fa0d0cdb0e28c80117aea57d305947f72
Allex Rocha <allexmmr@gmail.com>
2022-08-22 22:13:20 +1000

    Add Common project

---
+++ TestProject.Common/Utils/Encryptor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TestProject.Common.Utils
+{
+    public class Encryptor
+    {
+        public static string EncryptMD5(string input)
+        {
+            const string saltKey = "Z1pC0";
+            string encrypted = string.Empty;
+
+            using (var md5 = MD5.Create())
+            {
+                UTF8Encoding encoder = new UTF8Encoding();
+                byte[] hashedData = md5.ComputeHash(encoder.GetBytes(string.Concat(saltKey, input)));
+                encrypted = Convert.ToBase64String(hashedData);
+            }
+
+            return encrypted;
+        }
+    }
+}
